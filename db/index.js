443255ddd9cd77c1b40649e6f63a4cd920b0c417
John Hysong <johnahysong@gmail.com>
2022-04-30 23:52:43 -0400

    updated index.js and inquirer prompts

--- db/index.js
+++ db/index.js
@@ -9,16 +9,37 @@ function mainMenu() {
             type: "list",
             name: "choice",
             choices: [
+                // view Employees
+
                 {
                     name: "View ALL Employees",
                     value: "VIEW_EMPLOYEES"
                 },
-                // view Employees
                 // view Departments
+                {
+                    name: "View Departments",
+                    value: "VIEW_DEPARTMENTS"
+                },
                 // view Roles
+                {
+                    name: "View Roles",
+                    value: "VIEW_ROLES"
+                },
                 // add Employee
+                {
+                    name: "Add Employee",
+                    value: "ADD_EMPLOYEE"
+                },
                 // add Department
+                {
+                    name: "Add Department",
+                    value: "ADD_DEPARTMENT"
+                },
                 // update a employee role
+                {
+                    name: "Update Employee Role",
+                    value: "UPDATE_ROLE"
+                },
                 {
                     name: "Quit",
                     value: "QUIT"
@@ -29,5 +50,20 @@ function mainMenu() {
         let choices = res.choices;
         // now we call the appriopriate function depending on what the user chooses
         // how would we organize this?
+        // if conditional?
+        // switch/case?
+        // when?
     })
+}
+
+// conditional statements here - call cooresponding function
+
+async function viewEmployees() {
+    db.findAllEmployees()
+    .then(([rows]) => {
+        let employees = rows;
+        console.log("\n");
+        console.table(employees)
+})
+.then(() => mainMenu());
 }
