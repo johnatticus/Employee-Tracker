b841a3871d6c73dca9e6fb0e9c60416759468ad1
John Hysong <johnahysong@gmail.com>
2022-04-30 23:38:44 -0400

    added to schema and seeds. added boilerplate for connection.js

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "rootroot",
+    database: "employees"
+});
+
+// set up error handling in case the connection fails/breaks
+connection.connect(function (err) {
+    if (err) throw (err);
+});
+
+
+module.exports = connection;
--- db/seeds.sql
+++ db/seeds.sql
@@ -11,11 +11,25 @@ VALUES
 ("Engineering");
 
 INSERT INTO role
-()
+(title, salary, department_id)
 VALUES
-()
+("Senior Developer, 100000, 1"),
+("HR Business Partner, 150000, 1"),
+("Operations Manager, 100000, 1"),
+("Lawyer, 90000, 1");
+
+
 
 INSERT INTO employees
-()
+(first_name, last_name, role_id)
 VALUES
-()
+("Thad", "Jarvis", 1),
+("Dee", "Reynolds", 2),
+("Tairy", "Green", 3),
+("Jan", "Skylar", 4),
+("Barty", "Crouch Jr", 5),
+("Chip", "Baskets", 6),
+("Bill", "Adama", 7),
+("Jack", "Decker", 8),
+("Steve", "Mahanahan", 9),
+("Steve", "Brule", 10);
